22f2b39e68c5656a9d4a020fd822ea9c3fa18f47
Mohd Anas Ansari <mohdanas615@gmail.com>
2024-11-16 11:54:18 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Sidebar from './components/Sidebar'
 
 
 function App() {
   return (
     <>
+    {/* <Sidebar/> */}
 <Dashboard/>
     </>
     
